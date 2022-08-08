dac4e4501dee1d25a9b5028072edac1485e9c220
stuartgraham <stuart@stuart-graham.com>
2022-08-08 12:25:40 +0100

    Fix

--- create-new-vm.ps1
+++ create-new-vm.ps1
@@ -1,15 +1,15 @@
 $PSNativeCommandUseErrorActionPreference = $true
 $ErrorActionPreference = 'Stop'
 
-$refVM = "docker1"
-$refVM = Get-VM $refVM
+$templateVM = "ubuntu-2004-template"
+$refVM = Get-VM $templateVM
 $switch = (Get-VMNetworkAdapter -VMName $refVM.name).SwitchName
-Write-Host $switch
+Write-Host $switch[0]
 $firmware = Get-VMFirmware $refVM.name
 $newVM = Read-Host "Enter new hostname"
 
 Write-Host "Shutting down template machine"
-Stop-VM ubuntu-2004-template 
+Stop-VM $templateVM 
 
 Write-Host "Copying VHDX"
 $Source = "V:\VM\Virtual Hard Disks\ubuntu-template.vhdx"
@@ -27,4 +27,4 @@ Set-VMFireware $newVM -SecureBootTemplate $firmware.SecureBootTemplate
 Write-Host "Starting new VM"
 Start-VM $newVM
 Write-Host "Starting template VM"
-Start-VM ubuntu-2004-template
+Start-VM $templateVM
